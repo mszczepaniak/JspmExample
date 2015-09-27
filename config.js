0312fe7b08a266d613969e1f179dc499eb287311
Michal Szczepaniak <mr.szczepaniak@gmail.com>
2015-09-27 07:55:06 +0200

    Change transpiler to typescript

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 System.config({
   baseURL: "/",
   defaultJSExtensions: true,
-  transpiler: "babel",
+  transpiler: "typescript",
   babelOptions: {
     "optional": [
       "runtime",
@@ -17,6 +17,7 @@ System.config({
     "babel": "npm:babel-core@5.8.25",
     "babel-runtime": "npm:babel-runtime@5.8.25",
     "core-js": "npm:core-js@1.2.0",
+    "typescript": "npm:typescript@1.6.2",
     "github:jspm/nodelibs-process@0.1.1": {
       "process": "npm:process@0.10.1"
     },
